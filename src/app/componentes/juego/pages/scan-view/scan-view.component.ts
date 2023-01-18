07b83526f609968809a4ad7f306b61aeaa13d12e
Diego Rojas <Diego-27ro@outlook.com>
2023-01-18 15:56:48 -0500

    solve testing error

--- src/app/componentes/juego/pages/scan-view/scan-view.component.ts
+++ src/app/componentes/juego/pages/scan-view/scan-view.component.ts
@@ -36,10 +36,10 @@ export class ScanViewComponent implements OnInit {
 	async continueToGame() {
 		// console.log("code:" + this.keyController.getCode())
 		if (this.keyController.getCode() != "") {
-			// let validateTicket = this.gameLogic.verifyTicket(this.keyController.getCode())
+			let validateTicket = this.gameLogic.verifyTicket(this.keyController.getCode())
 			console.log("Codigo ingresado", this.code)
-			// let validateTicket = this.gameLogic.verifyTicket(this.keyController.getCode())
-			let validateTicket = this.gameLogic.verifyTicket("115727094")
+
+			// let validateTicket = this.gameLogic.verifyTicket("115727094")
 			if (await validateTicket) {
 				console.log("dentro del if" + validateTicket)
 				this.router.navigate(["/juego/play"])
