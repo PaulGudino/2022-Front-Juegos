473d57bca91c2f1cf06939b13be75d2398132e17
roberto encalada <rocaenca@espol.edu.ec>
2024-07-17 01:32:45 -0500

    Juego de puertas implementado

--- src/app/componentes/juego/pages/puertas-view/puertas-view.component.ts
+++ src/app/componentes/juego/pages/puertas-view/puertas-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { DashboardPublicityService } from 'src/app/servicios/publicity/dashboardPublicity/dashboard-publicity.service';
 import { DashboardStyleService } from 'src/app/servicios/theme/dashboardStyle/dashboard-style.service';
 import { AnimationGameService } from '../../service/animationGame/animation-game.service';
@@ -7,15 +7,8 @@ import { ThemeService } from '../../service/theme/theme.service';
 import { PublicityGameService } from 'src/app/servicios/publicityGame/publicity-game.service';
 import { GameLogicService } from '../../service/gameLogic/game-logic.service';
 import { ProbabilityService } from 'src/app/servicios/probability/probability/probability.service';
-
-//Servicios para generalizar el juego
-import { GameCurrentSessionService } from "src/app/servicios/gameCurrentSession/game-current-session.service"
+import { GameCurrentSessionService } from 'src/app/servicios/gameCurrentSession/game-current-session.service';
 import { GameCurrentSession_Data } from 'src/app/interfaces/gameCurrentSession/gamecurrentsession_data';
-import { ActivatedRoute } from "@angular/router"; // Importar ActivatedRoute
-import { PublicityGame } from "src/app/interfaces/publicityGame/PublicityGame"
-import { Audio } from "src/app/interfaces/audio/Audio"
-import { AudioService } from "src/app/servicios/audio/audio.service"
-import { ConfirmDialogService } from "src/app/servicios/confirm-dialog/confirm-dialog.service"
 
 @Component({
   selector: 'app-puertas-view',
@@ -26,29 +19,23 @@ export class PuertasViewComponent implements OnInit {
   backArrowEnabled: boolean = true;
   informationText: string = "A JUGAR!";
   slot_music = false;
-  attemps = 0;
+  attemps = 3; // Número de intentos permitidos
   resultMessage = '';
+  isOpening = false;
+  isGameStarted = false;
+  isDoorSelected = false; // Variable para controlar si una puerta ha sido seleccionada
   doors = [
-    { isOpen: false, prize: 'Premio 1', image: './assets/img/diseno_puerta.png' },
-    { isOpen: false, prize: 'Premio 2', image: './assets/img/diseno_puerta.png'  },
-    { isOpen: false, prize: 'Premio 3', image: './assets/img/diseno_puerta.png'  }
+    { isOpen: false, prize: 'Premio 1', image: '/assets/img/diseno_puerta.png' },
+    { isOpen: false, prize: 'Premio 2', image: '/assets/img/diseno_puerta.png'  },
+    { isOpen: false, prize: 'Premio 3', image: '/assets/img/diseno_puerta.png'  }
   ];
 
-  gameId: number | undefined; // Variable para almacenar game.id
-	gamecurrentsession: GameCurrentSession_Data | undefined;
-  // audio = new Audio()
-  // audioArray: Audio[] = []
-
-  probability: any = {
-    // id: 1,
-    // porcent_win: 20,
-    // winners_limit: 1,
-    // attempts_limit: 1,
-    // created: "2022-11-29T14:47:30.056806",
-    // modified: "2022-11-29T14:47:30.056806",
-    // is_active: true,
-    // game_id: 1
-  }
+  gameId: number | undefined;
+  gamecurrentsession: GameCurrentSession_Data | undefined;
+  probability: any = {};
+  currentDoor: number | null = null;
+  finalTransform: string = 'rotateY(0deg)';
+  openTime: number = 0;
 
   constructor(
     private router: Router,
@@ -60,83 +47,90 @@ export class PuertasViewComponent implements OnInit {
     public gameLogicService: GameLogicService,
     private probalilitySrv: ProbabilityService,
     private gameCurrentSessionService: GameCurrentSessionService,
-    private route: ActivatedRoute, // Inyectar ActivatedRoute
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
-			this.gameId = +params['gameId']; // Convertir el parámetro a número
-			if (!this.gameId) {
-				console.error('No gameId found in query params.');
-			} else {
-				// Llama a cualquier función que necesite usar gameId aquí, si es necesario
-				this.loadGameData();
-				this.updateGameIdForSession(this.gameId.toString());
-				this.loadCurrentJuego(this.gameId.toString());//el 1 representa la MAQUINA 1
-			}
-		});
-
-/*     this.probalilitySrv.getProbabilites().subscribe(
-      (data: any) => {
-        this.attemps = data.attempts_limit;
+      this.gameId = +params['gameId'];
+      if (!this.gameId) {
+        console.error('No gameId found in query params.');
+      } else {
+        this.loadGameData();
+        this.updateGameIdForSession(this.gameId.toString());
+        this.loadCurrentJuego(this.gameId.toString());
       }
-    ); */
-
+    });
   }
 
-    //Funciones usadas en el transcurso del Juego
-	loadGameData(): void {
-		// Aquí puedes cargar datos relacionados con gameId si es necesario
-		console.log(`Game ID: ${this.gameId}`);
-	}
+  loadGameData(): void {
+    console.log(`Game ID: ${this.gameId}`);
+  }
 
-	updateGameIdForSession(gameId: string): void {
-		const kiosko_numero = '1';
-		this.gameCurrentSessionService.updateGameId(kiosko_numero, gameId).subscribe(
-			(response) => {
-				console.log('Game ID updated successfully:', response);
-			},
-			(error) => {
-				console.error('Error updating Game ID:', error);
-			}
-		);
-	}
+  updateGameIdForSession(gameId: string): void {
+    const kiosko_numero = '1';
+    this.gameCurrentSessionService.updateGameId(kiosko_numero, gameId).subscribe(
+      (response) => {
+        console.log('Game ID updated successfully:', response);
+      },
+      (error) => {
+        console.error('Error updating Game ID:', error);
+      }
+    );
+  }
 
-	async loadCurrentJuego(gameId: string) {
-		try {
-		  const juegoSeleccionado = await this.gameLogicService.verifyGameCurrent(gameId);
-		  if (juegoSeleccionado) {
-			this.gamecurrentsession = juegoSeleccionado;
-			console.log('Detalle de Juego Actual:', this.gamecurrentsession);
-		  } else {
-			console.error('No se encontró juego .');
-		  }
-		} catch (error) {
-		  console.error('Error al cargar el juego :', error);
-		}
-	}
+  async loadCurrentJuego(gameId: string) {
+    try {
+      const juegoSeleccionado = await this.gameLogicService.verifyGameCurrent(gameId);
+      if (juegoSeleccionado) {
+        this.gamecurrentsession = juegoSeleccionado;
+        console.log('Detalle de Juego Actual:', this.gamecurrentsession);
+      } else {
+        console.error('No se encontró juego.');
+      }
+    } catch (error) {
+      console.error('Error al cargar el juego:', error);
+    }
+  }
+
+  startGame(): void {
+    this.isGameStarted = true;
+    this.isDoorSelected = false;
+  }
 
   openDoor(index: number): void {
-    if (this.attemps > 0 && !this.doors[index].isOpen) {
-      this.doors[index].isOpen = true;
-      this.resultMessage = `¡Felicidades! Ganaste ${this.doors[index].prize}.`;
-      this.attemps -= 1;
+    if (this.isGameStarted && this.attemps > 0 && !this.doors[index].isOpen && !this.isOpening && !this.isDoorSelected) {
+      this.isOpening = true;
+      this.isDoorSelected = true;
+      this.currentDoor = index;
+      this.openTime = Math.random() * 1 + 1;
+      
+      setTimeout(() => {
+        this.doors[index].isOpen = true;
+        this.resultMessage = `¡Felicidades! Ganaste ${this.doors[index].prize}.`;
+        this.attemps -= 1;
+        this.isOpening = false;
+        this.finalTransform = this.getTransform();
+      }, this.openTime * 100);
     } else if (this.attemps <= 0) {
       this.resultMessage = "No tienes más intentos.";
     }
   }
 
-    doSomething() {
-      sessionStorage.removeItem("juego_puertas");
+  getTransform() {
+    return 'rotateY(180deg)';
+  }
+
+  doSomething() {
+    sessionStorage.removeItem("juego_puertas");
+  }
+
+  music() {
+    if (this.attemps > 0) {
+      this.slot_music = true;
+      setTimeout(() => {
+        this.slot_music = false;
+      }, this.openTime * 10000);
     }
-    
-    music() {
-      if (this.attemps > 0) {
-        this.slot_music = true;
-        this.attemps -= 1;
-        setTimeout(() => {
-          this.slot_music = false;
-        }, 7000);
-      }
-    }
+  }
 }
