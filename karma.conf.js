e1ef6e799a9bf601fbcc05b1d053432878cf957d
Jefferson Eras <85094798+JeffErasLindao@users.noreply.github.com>
2024-08-09 01:34:34 -0500

    Se hizo Update karma.conf.js se reemplazo con ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -36,9 +36,11 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    //autoWatch: true,
+    //browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    //singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
