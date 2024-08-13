72a0fca1bf8222afed0dae43e3424cb4cacd7fd2
roberto encalada <rocaenca@espol.edu.ec>
2024-08-12 23:36:09 -0500

    cambiado info del ticket

--- src/app/componentes-compartidos/componentes-compartidos/tickect-qr/tickect-qr.component.ts
+++ src/app/componentes-compartidos/componentes-compartidos/tickect-qr/tickect-qr.component.ts
@@ -18,6 +18,8 @@ export class TickectQRComponent implements OnInit {
   @Input() codigoQR: string = '';
   @Input() QRdata: string = '';
 
+  @Input() fechaFormateada: string = '';
+
   constructor(
     private GameSrv: GameService,
     private QrConfigSrv: TicketConfigurationService
@@ -35,6 +37,16 @@ export class TickectQRComponent implements OnInit {
       this.title = res.title;
       this.description = res.description;
     });
+
+    // Obtener la fecha actual
+    const fechaActual = new Date();
+
+    // Formatear la fecha en un formato legible (por ejemplo, "DD/MM/YYYY")
+    const dia = String(fechaActual.getDate()).padStart(2, '0');
+    const mes = String(fechaActual.getMonth() + 1).padStart(2, '0'); // Los meses comienzan desde 0
+    const anio = fechaActual.getFullYear();
+
+    this.fechaFormateada = `${dia}/${mes}/${anio}`;
   }
 
 }
